4350e778a05db524ef656491a77a882d8627410c
jrhooker <jeff@jeffhooker.com>
2024-11-18 16:50:58 -0800

    Update launch.sh

--- batchfiles/launch.sh
+++ batchfiles/launch.sh
@@ -1,9 +1,9 @@
-export PATHTOPROJECT=\Source
-export OUTPUTPATH=\out
+export PATHTOPROJECT=/Source
+export OUTPUTPATH=/out
 export FILENAME=titan_regs.ditamap
 export DITAMAPNAME=titan_regs.ditamap
 
-cd ..\
+cd ../
 
 export WORKINGDIR=$PWD
 
@@ -17,31 +17,31 @@ echo $OUTPUTPATH
 echo $FILENAME
 echo $DITAMAPNAME
 
-cd $WORKINGDIR\batchfiles
+cd $WORKINGDIR/batchfiles
 
-mkdir $WORKINGDIR\out\
+mkdir $WORKINGDIR/out/
 
-java -cp $WORKINGDIR/depend/tools/saxon9/saxon9he.jar;$WORKINGDIR\depend\tools\Saxon9\xml-commons-resolver-1.2.jar ^
--Dxml.catalog.files=..\depend\tools\Saxon9\catalog.xml ^
+java -cp $WORKINGDIR/depend/tools/saxon9/saxon9he.jar;$WORKINGDIR/depend/tools/Saxon9/xml-commons-resolver-1.2.jar ^
+-Dxml.catalog.files=../depend/tools/Saxon9/catalog.xml ^
 net.sf.saxon.Transform ^
 -r:org.apache.xml.resolver.tools.CatalogResolver ^
 -x:org.apache.xml.resolver.tools.ResolvingXMLReader ^
 -y:org.apache.xml.resolver.tools.ResolvingXMLReader ^
--o:$WORKINGDIR$OUTPUTPATH\test.xml ^
--s:$WORKINGDIR\$PATHTOPROJECT\$FILENAME ^
--xsl:$WORKINGDIR\depend\custom\generate_ditamap.xsl ^
+-o:$WORKINGDIR$OUTPUTPATH/test.xml ^
+-s:$WORKINGDIR/$PATHTOPROJECT/$FILENAME ^
+-xsl:$WORKINGDIR/depend/custom/generate_ditamap.xsl ^
 DITAMAPNAME="$DITAMAPNAME" PATHTOPROJECT="$PATHTOPROJECT" OUTPUTPATH="$OUTPUTPATH" WORKINGDIR="$WORKINGDIR" 
   
   
-java -cp $WORKINGDIR/depend/tools/saxon9/saxon9he.jar;$WORKINGDIR\depend\tools\Saxon9\xml-commons-resolver-1.2.jar ^
--Dxml.catalog.files=..\depend\tools\Saxon9\catalog.xml ^
+java -cp $WORKINGDIR/depend/tools/saxon9/saxon9he.jar;$WORKINGDIR/depend/tools/Saxon9/xml-commons-resolver-1.2.jar ^
+-Dxml.catalog.files=../depend/tools/Saxon9/catalog.xml ^
 net.sf.saxon.Transform ^
 -r:org.apache.xml.resolver.tools.CatalogResolver ^
 -x:org.apache.xml.resolver.tools.ResolvingXMLReader ^
 -y:org.apache.xml.resolver.tools.ResolvingXMLReader ^
--o:$WORKINGDIR$OUTPUTPATH\test.xml ^
--s:$WORKINGDIR\$PATHTOPROJECT\$FILENAME ^
--xsl:$WORKINGDIR\depend\custom\generate_topics.xsl ^
+-o:$WORKINGDIR$OUTPUTPATH/test.xml ^
+-s:$WORKINGDIR/$PATHTOPROJECT/$FILENAME ^
+-xsl:$WORKINGDIR/depend/custom/generate_topics.xsl ^
 PATHTOPROJECT="$PATHTOPROJECT" OUTPUTPATH="$OUTPUTPATH" WORKINGDIR="$WORKINGDIR" 
 
  
