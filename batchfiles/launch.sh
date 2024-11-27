a6740f8ade4c2f422b9640ea85b6d21bdeba9170
jrhooker <jeff@jeffhooker.com>
2024-11-27 12:45:55 -0800

    Update launch.sh

--- batchfiles/launch.sh
+++ batchfiles/launch.sh
@@ -22,26 +22,6 @@ cd $WORKINGDIR/batchfiles
 mkdir $WORKINGDIR/out/
 
 java -cp $WORKINGDIR/depend/tools/saxon9/saxon9he.jar;$WORKINGDIR/depend/tools/Saxon9/xml-commons-resolver-1.2.jar ^
--Dxml.catalog.files=../depend/tools/Saxon9/catalog.xml ^
-net.sf.saxon.Transform ^
--r:org.apache.xml.resolver.tools.CatalogResolver ^
--x:org.apache.xml.resolver.tools.ResolvingXMLReader ^
--y:org.apache.xml.resolver.tools.ResolvingXMLReader ^
--o:$WORKINGDIR$OUTPUTPATH/test.xml ^
--s:$WORKINGDIR/$PATHTOPROJECT/$FILENAME ^
--xsl:$WORKINGDIR/depend/custom/generate_ditamap.xsl ^
-DITAMAPNAME="$DITAMAPNAME" PATHTOPROJECT="$PATHTOPROJECT" OUTPUTPATH="$OUTPUTPATH" WORKINGDIR="$WORKINGDIR" 
-  
-  
-java -cp $WORKINGDIR/depend/tools/saxon9/saxon9he.jar;$WORKINGDIR/depend/tools/Saxon9/xml-commons-resolver-1.2.jar ^
--Dxml.catalog.files=../depend/tools/Saxon9/catalog.xml ^
-net.sf.saxon.Transform ^
--r:org.apache.xml.resolver.tools.CatalogResolver ^
--x:org.apache.xml.resolver.tools.ResolvingXMLReader ^
--y:org.apache.xml.resolver.tools.ResolvingXMLReader ^
--o:$WORKINGDIR$OUTPUTPATH/test.xml ^
--s:$WORKINGDIR/$PATHTOPROJECT/$FILENAME ^
--xsl:$WORKINGDIR/depend/custom/generate_topics.xsl ^
-PATHTOPROJECT="$PATHTOPROJECT" OUTPUTPATH="$OUTPUTPATH" WORKINGDIR="$WORKINGDIR" 
+
 
  
